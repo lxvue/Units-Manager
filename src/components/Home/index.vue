29ed89589609205b0986032228a36d3e574a3fc0
lyy <15301170766@163.com>
2018-08-01 17:02:41 +0800

    1

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,12 +1,14 @@
 <template>
 	<div>
-	<el-container>
+	<el-container style="height: 100vh;">
 		<el-header>单位体质内管理</el-header>
 		<el-container>
-			<el-aside width="200px">Aside</el-aside>
+			<el-aside width="200px" >Aside</el-aside>
 			<el-container>
 				<el-main>Main</el-main>
-				<el-footer>Footer</el-footer>
+				<el-footer>
+					@技术支持:圆圆/雪萍
+				</el-footer>
 			</el-container>
 		</el-container>
 	</el-container>
@@ -22,30 +24,36 @@
     }
   </script>
   <style scoped="scoped" lang="less">
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
+
   
+ 
+  
+
+  
+  
+  .el-header{
+	background-color: #7ED8A9;
+	color: #333;
+	text-align: center;
+	line-height:  60px; 
+	//height: 200px !important;
+  }
   .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  height: calc(100vh - 60px);
   }
-  
   .el-main {
-    background-color: #E9EEF3;
+    background-color: white;
     color: #333;
     text-align: center;
     line-height: 160px;
   }
-  
-  body > .el-container {
-    margin-bottom: 40px;
+  .el-footer {
+  background-color: gray;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
   }
-  
-
 </style>
