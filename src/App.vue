b680acfdd1118ea15da3b74e75b4accaa6b1fe64
15301170766 <15301170766@163.com>
2018-08-02 01:20:17 +0800

    添加user

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,16 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 *{
 	padding: 0;
 	margin: 0;
+}
+/* 小手提示 */
+.pop_pointer{
+	cursor:pointer
 }
 </style>
