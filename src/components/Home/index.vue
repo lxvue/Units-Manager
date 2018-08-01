e3fecb398738b1bc09b0a44657f160f884e1b6ac
lyy <15301170766@163.com>
2018-08-01 16:30:28 +0800

    popup_app_css

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 	<el-container>
-		<el-header>Header</el-header>
+		<el-header>单位体质内管理</el-header>
 		<el-container>
 			<el-aside width="200px">Aside</el-aside>
 			<el-container>
@@ -21,7 +21,7 @@
       }
     }
   </script>
-  <style>
+  <style scoped="scoped" lang="less">
   .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
@@ -47,12 +47,5 @@
     margin-bottom: 40px;
   }
   
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+
 </style>
